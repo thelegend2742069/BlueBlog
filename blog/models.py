165288dcd6c7d8b581e2d681af7939e69ef42005
Pranav Shirole <blab8650@gmail.com>
2023-09-27 00:55:12 +0530

    added post database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateField(default=timezone.now)
+    
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Pranav',
-        'title': 'My First Post',
-        'content': 'Post Number 1',
-        'date_posted': 'September 26, 2023'
-    },
-    
-    {
-        'author': 'Spiderman',
-        'title': 'AAAAAAAAAAAAAAAAAAAAAAA',
-        'content': 'spiders are fucking terrifying\nwhy did I have to become a spider\nwhy not something nicer like a butterflys',
-        'date_posted': 'September 26, 2023'
-    },
-]
 
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
 
     return render(request, 'blog/home.html', context)
 
